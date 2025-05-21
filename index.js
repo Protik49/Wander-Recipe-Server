495ae6938366418c30b78ced727ad806e1cc10bc
Protik49 <protikprotik49@gmail.com>
2025-05-22 05:55:53 +0600

    added multiple get and post apis

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = 3000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //MongoDB password: edRG5rxqUeq0Ds9G
 const recipes = require("./recipe");
@@ -11,8 +11,6 @@ console.log(recipes[3].preparationTime);
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhaekj1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -46,6 +44,27 @@ async function run() {
       const result = await recipeCollection.find().toArray();
       res.send(result);
     });
+
+    app.get("/all-recipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await recipeCollection.findOne(query);
+      res.send(result);
+    });
+
+      app.get("/recipes/:email", async (req, res) => {
+          const email = req.params.email;
+          const result = await recipeCollection.find({ email: email }).toArray();
+          res.send(result);
+    })
+
+    app.post("/recipes", async (req, res) => {
+      const recipe = req.body;
+      const result = await recipeCollection.insertOne(recipe);
+      res.send(result);
+    });
+
+      
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
